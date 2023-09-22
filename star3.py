4e12ac3c95ff4afac62163ba04a9d8e7ba7f2cf8
junyeokk <cjh4302@gmail.com>
2023-09-22 23:27:14 +0900

    V6:왼쪽 역삼각형 리팩토링

--- star3.py
+++ star3.py
@@ -1,7 +1,7 @@
 # star3.py
 
-print(" * * * * * ")
-print(" * * * *   ")
-print(" * * *     ")
-print(" * *       ")
-print(" *         ")
+# for문이용구현
+
+N = 6
+for i in range(N-1, 0, -1):
+    print(" *" * i + " "*(N-i))
