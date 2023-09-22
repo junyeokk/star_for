50f3c209eb76a0ef2db839ca5f6d6a84c92e92b3
junyeokk <cjh4302@gmail.com>
2023-09-22 23:26:05 +0900

    V4: 오른쪽 삼각형 리팩토링

--- star2.py
+++ star2.py
@@ -1,7 +1,7 @@
 # star2.py
 
-print("         * ")
-print("       * * ")
-print("     * * * ")
-print("   * * * * ")
-print(" * * * * * ")
+# for문이용구현
+
+N = 6
+for i in range(N-1, 0, -1):
+    print(" "*(2*i-1) + " *"*(N-i))
